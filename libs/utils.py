0c64f3a32fcd459097e8bd1ca15e8cc897e38c29
shaotao002 <403592593@qq.com>
2021-05-31 15:01:56 +0800

    配置文件修改、参数设置接口、回执点设置
    1、配置文件修改和添加参数设置配置；
    2、修改参数设置接口；
    3、回执点数据格式从字典改为数组；

--- libs/utils.py
+++ libs/utils.py
@@ -10,6 +10,7 @@ import time, base64, os, random
 import logging
 import subprocess
 import datetime
+from libs.globleConfig import SCAN_SET
 
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('django')
@@ -196,14 +197,15 @@ def base64_turn_png(str_base64, path=None, image_type="avatar"):
 def set_scan_parameter(set_params):
     # ['calc', 'mspaint', 'notepad', 'write', 'osk'] ['calc', r'mkdir C:\Users\peiqi\Desktop\1_conver\test2']
     # start = datetime.datetime.now()
-    for cmd in ['Date']:
+    for cmd_v in set_params.values():
         try:
-            cut_process = subprocess.Popen(cmd, shell=True)
-            a = cut_process.communicate()
-            print('打印结果=>:', a)  # TODO：获取修改后的值, 需要添加是否修改成功判断, 如果没修改成功需要，再次修改-直到成功为止
+            print('cmd_v--->:', SCAN_SET[cmd_v])
+            # cut_process = subprocess.Popen(cmd_v, shell=True)
+            # a = cut_process.communicate()
+            # print('打印结果=>:', a)  # TODO：获取修改后的值, 需要添加是否修改成功判断, 如果没修改成功需要，再次修改-直到成功为止
         except Exception as e:
-            # print("？？？？？？？？？？？？===== process timeout 执行失败结束进程 ======")
-            cut_process.kill()
+            print("？？？？？？？？？？？？===== process timeout 执行失败结束进程 ======")
+            # cut_process.kill()
     # sleep(0.3)  # TODO: 等待一秒钟检查是否已经修改参数
     # end = datetime.datetime.now()
     # print('花费时间=>:', end - start)
